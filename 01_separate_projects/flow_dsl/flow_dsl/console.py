633d36e970316c87c7e08074db2c8954538a41e4
goto40 <pierre.bayerl@googlemail.com>
2019-03-29 19:45:34 +0100

    initial inspector

--- 01_separate_projects/flow_dsl/flow_dsl/console.py
+++ 01_separate_projects/flow_dsl/flow_dsl/console.py
@@ -1,11 +1,28 @@
 from textx.scoping import MetaModelProvider
 import click
 import flow_dsl
-
+import textx_inspector
 
 ##############################################
 #  overrides validate command from types_dsl
 ##############################################
+
+@click.argument('model_files', nargs=-1)
+def inspect(model_files):
+    """
+    This command validates *.flow, *.data or *.type-files.
+    """
+    flow_dsl.get_metamodel_flow()  # activate/register meta model
+    root = None
+    for filename in model_files:
+        try:
+            mm = MetaModelProvider.get_metamodel(None, filename)
+            m = mm.model_from_file(filename)
+            root = textx_inspector.inspect(m, root, False)
+        except BaseException as e:
+            print('  WARNING/ERROR: {}'.format(e))
+    if root is not None:
+        root.mainloop()
 
 
 @click.argument('model_files', nargs=-1)
