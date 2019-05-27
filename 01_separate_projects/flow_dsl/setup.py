520c4fe2ca7751ed6b69f4551f2dc6c8143c03e5
goto40 <pierre.bayerl@googlemail.com>
2019-05-27 19:15:33 +0200

    review comments: fixed dependecies

--- 01_separate_projects/flow_dsl/setup.py
+++ 01_separate_projects/flow_dsl/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='flow_dsl',
       packages=find_packages(),
       package_data={'': ['*.tx']},
-      install_requires=["textx"],
+      install_requires=["textx", "data_dsl", "types_dsl"],
       entry_points={
         'textx_languages': [
             'flow_dsl = flow_dsl:flow_dsl',
